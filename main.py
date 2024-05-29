91010f080a8900f61cbc85d126f404616e013cf4
43a1723 <hai1723notgay@gmail.com>
2024-05-29 09:11:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def compile_and_marshal(input_file, output_file):
 
     # Tuần tự hóa bytecode bằng marshal
     marshalled_code = marshal.dumps(compiled_code)
-    marshalled_code = f'exec(marshal.loads({repr(marshalled_code)}))'
+    import = "import marshal ;"
+    marshalled_code = f'{import}exec(marshal.loads({repr(marshalled_code)}))'
 
     # Ghi mã đã tuần tự hóa vào tệp mới
     with open(output_file, 'wb') as file:

4ed7591e4ad2dd7029e2c21470732849c493e08f
43a1723 <hai1723notgay@gmail.com>
2024-06-18 15:08:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -109,26 +109,28 @@ def dell(code):
 
 
 code = f"""
-import marshal
-import base64
 {encryptcode2(xor_exec)}
 exec(xor_encrypt_decrypt("{string_to_hex(xor_encrypt_decrypt(encryptcode2(defcode)))}"))
 exec(xor_encrypt_decrypt("{string_to_hex(xor_encrypt_decrypt(encryptcode2(execcode)))}"))
 """
 
 abcdefghijklmnopqrstuvwxyz = base64.b64encode("abcdefghijklmnopqrstuvwxyz".encode('utf-8')).decode('utf-8')
-encoded_nopqrstuvwxyzabcdefghijklm = base64.b64encode(nopqrstuvwxyzabcdefghijklm.encode('utf-8')).decode('utf-8')
+encoded_nopqrstuvwxyzabcdefghijklm = base64.b64encode(nopqrstuvwxyzabcdefghijklm1.encode('utf-8')).decode('utf-8')
 
-defcode = f"""
-{execcodevar}
-
-def dell(code):
+protectcode = f"""
+def delll(code):
     letter = base64.b64decode("{abcdefghijklmnopqrstuvwxyz}").decode('utf-8') + base64.b64decode("{abcdefghijklmnopqrstuvwxyz}").decode('utf-8').upper()
     nopqrstuvwxyzabcdefghijklm = base64.b64decode("{encoded_nopqrstuvwxyzabcdefghijklm}").decode('utf-8')
     letter2 = nopqrstuvwxyzabcdefghijklm
     obfuscated_code = code.translate(str.maketrans(letter2, letter))
     return obfuscated_code
+exec(delll(code))
 """
-
+protectcode = f"""
+import marshal
+import base64
+code = "{string_to_hex(t(pyminify(code)))}"
+{encryptcode2(protectcode)}
+"""
 with open('output.txt', 'w') as file:
-    file.write(pyminify(code))
+    file.write(pyminify(protectcode))

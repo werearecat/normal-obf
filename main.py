341c44e47d212734f8ce762152ad4173df115911
43a1723 <hai1723notgay@gmail.com>
2024-07-02 18:25:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import marshal
 import random
 import base64
 from python_minifier import minify as pyminify
+from need.superobf import *
 import need
 
 nopqrstuvwxyzabcdefghijklm = "abcdefghijklmnopqrstuvwxyz"
